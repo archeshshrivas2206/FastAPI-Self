890ceec3ea63be21208106e7a36849a81730673e
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-09-07 12:24:56 +0530

    middleware and cors done

--- middleware and cors/main.py
+++ middleware and cors/main.py
@@ -1,18 +1,48 @@
 import schemas
 import model
 import auth
+
 from database import Base, engine , get_db
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+import time
+from fastapi import Request
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app=FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="login")
+
+@app.middleware("http")
+async def log_requests(request:Request,call_next):
+    start_time=time.time()
+    response=await call_next(request)
+    duration=time.time()-start_time
+    print (f"{request.method}{request.url.path} completed in {duration:.4f}s")
+    return response
+
+@app.middleware("http")
+async def add_process_time_header(request: Request,call_next):
+    start_time=time.time()
+    response=await call_next(request)
+    process_time=time.time()- start_time
+    response.headers["X-Process-Time"]=str(process_time)
+    return response
 
 def get_current_user(token:str=Depends(oauth2_scheme),db:Session=Depends(get_db)):
     payload=auth.decode_access_token(token)
